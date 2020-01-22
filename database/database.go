0109aa6580ceb9cf98fcdb5473942940ec27ad32
Aleksei <a.gryczewski999@gmail.com>
2020-01-22 12:56:57 -0500

    Removed rethinkdb implementation

--- database/database.go
+++ database/database.go
@@ -1,68 +1,24 @@
 package database
 
-import (
-	"github.com/salmonllama/fsbot_go/lib"
-	r "gopkg.in/rethinkdb/rethinkdb-go.v6"
-	"log"
-)
-
 type Database struct {
-	Host string
-	Port string
-	Name string
-	User string
+	Host     string
+	Port     string
+	Name     string
+	User     string
 	Password string
-	Session *r.Session
-	Outfit *OutfitDB
+	Session  string
+	Outfit   *OutfitDB
 }
 
 func (db *Database) load() {
-	outfit := OutfitDB{Session:db.Session}
+	outfit := OutfitDB{}
 	db.Outfit = &outfit
 }
 
 func (db *Database) OpenConnection() *Database {
-	session, err := r.Connect(r.ConnectOpts{
-		Address:             db.Host + ":" + db.Port,
-		Database:            db.Name,
-		Username:            db.User,
-		Password:            db.Password,
-	})
-	lib.Check(err)
-	db.Session = session
-
-	db.load()
 	return db
 }
 
 func (db *Database) CreateTables() {
-	var (
-		result r.WriteResponse
-		err error
-	)
 
-	result, err = r.TableCreate("outfits").RunWrite(db.Session)
-	if err == nil {
-		log.Print(result)
-	}
-
-	result, err = r.TableCreate("guild_conf").RunWrite(db.Session)
-	if err ==  nil {
-		log.Print(result)
-	}
-
-	result, err = r.TableCreate("guild_blacklist").RunWrite(db.Session)
-	if err == nil {
-		log.Print(result)
-	}
-
-	result, err = r.TableCreate("user_blacklist").RunWrite(db.Session)
-	if err == nil {
-		log.Print(result)
-	}
-
-	result, err = r.TableCreate("color_roles").RunWrite(db.Session)
-	if err == nil {
-		log.Print(result)
-	}
 }
--- lib/types.go
+++ lib/types.go
@@ -2,8 +2,34 @@ package lib
 
 // Outfit hold information related to the outfits
 type Outfit struct {
-	ID        string
+	Id        string
 	Link      string
 	Submitter string
 	Tag       string
+	Featured  bool
+	Deleted   bool
+	Date      string
+}
+
+type GuildConf struct {
+	Id               string
+	Prefix           string
+	LogChannel       string
+	DisabledModules  map[string]bool
+	DisabledCommands map[string]bool
+}
+
+type UserBlacklist struct {
+	Id          string
+	Blacklisted bool
+	Reason      string
+}
+
+type GuildBlacklist struct {
+	Id          string
+	Blacklisted bool
+	Reason      string
+}
+
+type ColorRole struct {
 }
